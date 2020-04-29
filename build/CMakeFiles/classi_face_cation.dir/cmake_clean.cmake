3d050198edc4e7a4f096f66d6181af0c438f598c
ArtKomarov <Komarov.A@phystech.edu>
2020-04-30 00:51:39 +0300

    Add build

---
+++ build/CMakeFiles/classi_face_cation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classi_face_cation.dir/main.cpp.o"
+  "classi_face_cation.pdb"
+  "classi_face_cation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/classi_face_cation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
