cf082a195ad17fabdb9833655639da27a2633ffb
ViktorPalych <63556945+ViktorPalych@users.noreply.github.com>
2020-05-13 02:54:52 +0500

    Update clfc.hpp

--- clfc.hpp
+++ clfc.hpp
@@ -3,6 +3,8 @@
 
 typedef unsigned long int uli;
 
+const size_t CHANELS_NUM = 1;
+const size_t NUM_CLASSES = 7;
 const size_t PICTURE_HEIGHT = 48;
 const size_t PICTURE_WIDTH  = 48;
 const size_t PICTURE_SIZE   = PICTURE_HEIGHT * PICTURE_WIDTH;
