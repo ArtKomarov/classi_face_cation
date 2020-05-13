ab7f54f77f7216d8d8f09c4fcbba2dadf1ad0ef9
ViktorPalych <63556945+ViktorPalych@users.noreply.github.com>
2020-05-13 13:19:49 +0500

    Create clfc.hpp

---
+++ Nets/clfc.hpp
@@ -0,0 +1,12 @@
+#ifndef CLASSIFACECATION_HPP
+#define CLASSIFACECATION_HPP
+
+typedef unsigned long int uli;
+
+const size_t CHANELS_NUM = 1;
+const size_t NUM_CLASSES = 7;
+const size_t PICTURE_HEIGHT = 48;
+const size_t PICTURE_WIDTH  = 48;
+const size_t PICTURE_SIZE   = PICTURE_HEIGHT * PICTURE_WIDTH;
+
+#endif // CLASSIFACECATION_HPP
