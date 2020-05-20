8c1247cfdd7efcc0c310367e2cb80c279687e891
ArtKomarov <Komarov.A@phystech.edu>
2020-05-20 23:38:31 +0300

    feature request

--- main.cpp
+++ main.cpp
@@ -16,42 +16,66 @@
 
 using namespace torch;
 
-int main() {
-//    auto custom_dataset = CustomDataset("fer2013.csv", 0.3).map(data::transforms::Stack<>());
+int main(int argc, char* argv[]) { //
+    std::string data_file_name;
+    std::cout << "Try to give dataset name as first command line parametr...";
+    if(argc > 1) {
+        std::cout << "success" << std::endl;
+        try {
+            data_file_name = argv[1];
+        } catch (const std::bad_alloc& ba) {
+            std::cout << "Fail to allcoate memory for dataset file name."<< std::endl;
+            std::cerr << ba.what() << std::endl;
+        } catch (const std::length_error& le) {
+            std::cout << "File name is too large!" << std::endl;
+            std::cerr << le.what() << std::endl;
+        }
+    }
+    else {
+        std::cout << "failed" << std::endl;
+        std::cout << "Enter dataset file name:" << std::endl;
+        std::cin >> data_file_name;
+    }
 
-//    data_loader_t data_loader = data::make_data_loader<data::samplers::SequentialSampler>(
-//                std::move(custom_dataset),
-//                batch_size
-//                );
+    TrainTestData TTData(data_file_name, 0.3);
 
-    TrainTestData TTData("fer2013.csv", 0.3);
+    if(!TTData.good()) {
+        return -1;
+    }
 
-    int num;
+    int num = 0;
     auto net = std::make_shared<Net>();
     std::cout << "Enter the number:" << std::endl;
-    std::cout << "1 - AlexNet" << std::endl;
-    std::cout << "2 - OurNet" << std::endl;
-    std::cout << "3 - SmallNet" << std::endl;
-    std::cout << "4 - VGG16" << std::endl;
-    std::cout << "5 - ResNet18" << std::endl;
-    std::cin >> num;
-    switch (num)
+    std::cout << "1 - AlexNet"       << std::endl;
+    std::cout << "2 - OurNet"        << std::endl;
+    std::cout << "3 - SmallNet"      << std::endl;
+    std::cout << "4 - VGG16"         << std::endl;
+    std::cout << "5 - ResNet18"      << std::endl;
+    while (!num) // добавлено
     {
-    case 1:
-        net = std::make_shared<AlexNet>();
-        break;
-    case 2:
-        net = std::make_shared<OurNet>();
-        break;
-    case 3:
-        net = std::make_shared<SmallNet>();
-        break;
-    case 4:
-        net = std::make_shared<VGG16>();
-        break;
-    case 5:
-        net = std::make_shared<ResNet18>();
-        break;
+        std::cin >> num;
+        switch (num)
+        {
+        case 1:
+            net = std::make_shared<AlexNet>();
+            break;
+        case 2:
+            net = std::make_shared<OurNet>();
+            break;
+        case 3:
+            net = std::make_shared<SmallNet>();
+            break;
+        case 4:
+            net = std::make_shared<VGG16>();
+            break;
+        case 5:
+            net = std::make_shared<ResNet18>();
+            break;
+        default: // добавлено
+            num = 0; //
+            std::cout << "The number should be from 1 to 5" << std::endl; //
+            break; //
+        }
     }
 
 
@@ -100,7 +124,7 @@ int main() {
         save(net, "net3.pt");
     }
 
-    int stop = 0;
+    //int stop = 0;
     long int len = 0;
     Tensor accuracy;
     Tensor correct_elems = torch::zeros({1}, options);
@@ -117,15 +141,19 @@ int main() {
             len += BATCH_SIZE;
         }
         accuracy = correct_elems / torch::full(1, len, options);
-        std::cout << "Test accuracy: " << accuracy.item() << std::endl;
+        std::cerr << "Test accuracy: " << accuracy.item() << std::endl;
     } catch (const c10::IndexError& er){
-        std::cout << er.what() << std::endl;
+        std::cout << "Testing failed" << std::endl;
+        std::cerr << er.what() << std::endl;
     } catch (const c10::ValueError& vr){
-        std::cout << vr.what() << std::endl;
+        std::cout << "Testing failed" << std::endl;
+        std::cerr << vr.what() << std::endl;
     } catch (const std::runtime_error& re) {
-        std::cout << re.what() << std::endl;
+        std::cout << "Testing failed" << std::endl;
+        std::cerr << re.what() << std::endl;
     } catch(...) {
         std::cout << "Testing failed" << std::endl;
+        throw;
     }
 
     return 0;
